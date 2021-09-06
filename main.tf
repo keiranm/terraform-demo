a47359e62e87961b801df1dbb33e41e686aa47d0
Keiran McManus <kmcmanus3@dxc.com>
2021-09-06 14:03:56 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
+terraform {
+    backend "remote" {
+        organization = "kmcmanus3"
+        workspaces {
+            name = "terraform-demo"
+        }
+    }
+}
+
 provider "aws" {
-  region = "ap-southeast-2"
-  access_key = ""
-  secret_key = ""
+    region = "ap-southeast-2"
 }
 
 module "vpc_example_simple-vpc" {
-  source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
-  version = "3.7.0"
+    source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
+    version = "3.7.0"
 }
