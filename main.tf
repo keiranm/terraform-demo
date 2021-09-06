abc31c955d34f98dfe6c790854571be9349a7003
Keiran McManus <kmcmanus3@dxc.com>
2021-09-06 14:05:23 +1000

    Terraform fmt

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
-    backend "remote" {
-        organization = "kmcmanus3"
-        workspaces {
-            name = "terraform-demo"
-        }
+  backend "remote" {
+    organization = "kmcmanus3"
+    workspaces {
+      name = "terraform-demo"
     }
+  }
 }
 
 provider "aws" {
-    region = "ap-southeast-2"
+  region = "ap-southeast-2"
 }
 
 module "vpc_example_simple-vpc" {
-    source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
-    version = "3.7.0"
+  source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
+  version = "3.7.0"
 }
